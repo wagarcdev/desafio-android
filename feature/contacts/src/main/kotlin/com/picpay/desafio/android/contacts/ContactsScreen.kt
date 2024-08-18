e248f5b7adf6879b1e7feb9c8ad6b12e23395ec0
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-18 12:33:07 -0300

    feat: adds loading indicator and error toast to UI

--- feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/ContactsScreen.kt
+++ feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/ContactsScreen.kt
@@ -1,13 +1,20 @@
 package com.picpay.desafio.android.contacts
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,25 +26,44 @@ import com.picpay.desafio.android.feature.contacts.R
 fun ContactsScreen(
     uiState: ContactsScreenUiState
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
-    ) {
-        item {
-            Text(
-                text = stringResource(R.string.contacts_screen_list_header),
-                fontSize = 28.sp,
-                color = Color.White,
-                fontWeight = FontWeight.Bold
-            )
-        }
+    val context = LocalContext.current
 
-        items(
-            items = uiState.users,
-            key = { it.id }
-        ) { user ->
-            ContactItem(user)
+    LaunchedEffect(uiState.isError) {
+        if (uiState.isError != null) {
+            Toast.makeText(context, uiState.isError, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    if (uiState.isLoading) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            CircularProgressIndicator(color = Color.Green)
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .padding(24.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp)
+        ) {
+            item {
+                Text(
+                    text = stringResource(R.string.contacts_screen_list_header),
+                    fontSize = 28.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            items(
+                items = uiState.users,
+                key = { it.id }
+            ) { user ->
+                ContactItem(user)
+            }
         }
     }
 }
