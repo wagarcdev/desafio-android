17412196acbcff20ae2ba57c53e8659f7b1e2429
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-20 04:12:59 -0300

    fix: showing no internet on start

--- feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/ContactsScreenUiState.kt
+++ feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/ContactsScreenUiState.kt
@@ -4,10 +4,10 @@ import com.picpay.desafio.android.domain.model.UserModel
 
 data class ContactsScreenUiState(
     var isSyncing: Boolean = false,
-    var isNetworkAvailable: Boolean = false,
+    var isNetworkAvailable: Boolean? = null,
     var displayMessage: String? = null,
     var users: List<UserModel> = emptyList(),
 ){
     val showNoInternet: Boolean
-        get() = users.isEmpty() && !isNetworkAvailable
+        get() = users.isEmpty() && isNetworkAvailable?.not() ?: false
 }
