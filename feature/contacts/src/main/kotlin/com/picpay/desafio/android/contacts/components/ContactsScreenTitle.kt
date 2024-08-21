fc71b80eaa90280f4c95c986c0730a69c29d516f
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-21 13:09:03 -0300

    fix: adds title as area to clear text field focus

--- feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/components/ContactsScreenTitle.kt
+++ feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/components/ContactsScreenTitle.kt
@@ -1,10 +1,15 @@
 package com.picpay.desafio.android.contacts.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -13,9 +18,18 @@ import com.picpay.desafio.android.feature.contacts.R
 
 @Composable
 fun ContactsScreenTitle() {
+
+    val focusManager = LocalFocusManager.current
+    val interaction = remember { MutableInteractionSource() }
+
     Text(
         modifier = Modifier
-            .padding(vertical = 21.dp),
+            .padding(vertical = 21.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = interaction,
+                indication = null
+            ) { focusManager.clearFocus() },
         text = stringResource(R.string.contacts_screen_list_header),
         fontSize = 28.sp,
         color = Color.White,
