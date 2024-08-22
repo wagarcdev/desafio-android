cc0c721bcf8a4445a29046093d832812f623cd39
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-22 01:31:55 -0300

    fix: scroll position was destroyed on configuration changes

--- feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/components/contactlist/ContactsList.kt
+++ feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/components/contactlist/ContactsList.kt
@@ -11,7 +11,12 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
@@ -26,14 +31,25 @@ fun ContactsList(
     val focusManager = LocalFocusManager.current
     val interaction = remember { MutableInteractionSource() }
 
+    var lastFirstItem by rememberSaveable { mutableIntStateOf(0) }
+
     val listState = rememberLazyListState()
     LaunchedEffect(
         uiState.searchUiState.searchQuery,
         uiState.searchUiState.sortedBy,
         uiState.searchUiState.orderDirection,
     ) {
-        delay(50)
-        listState.scrollToItem(0)
+        delay(50) // Time for list recompose before trying to scroll
+        listState.animateScrollToItem(0)
+    }
+
+    LaunchedEffect(Unit) {
+        delay(55) // Time for list recompose before trying to scroll
+        listState.animateScrollToItem(lastFirstItem)
+    }
+    // Workaround to save scroll position during configuration changes
+    if (listState.isScrollInProgress) {
+        lastFirstItem = remember { derivedStateOf { listState.firstVisibleItemIndex } }.value
     }
 
     LazyColumn(
