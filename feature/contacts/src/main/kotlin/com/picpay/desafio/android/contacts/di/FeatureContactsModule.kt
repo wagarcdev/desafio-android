e418d4a4771a8105d34e5fcab6bfb4ea87b75403
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-23 12:24:15 -0300

    refactor: migrates viewmodel dependencies resolution to koin

--- feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/di/FeatureContactsModule.kt
+++ feature/contacts/src/main/kotlin/com/picpay/desafio/android/contacts/di/FeatureContactsModule.kt
@@ -2,20 +2,20 @@ package com.picpay.desafio.android.contacts.di
 
 import com.picpay.desafio.android.contacts.datasource.local.UserLocalDataSourceImpl
 import com.picpay.desafio.android.contacts.datasource.remote.UserRemoteDataSourceImpl
-import com.picpay.desafio.android.network.services.UserService
 import com.picpay.desafio.android.contacts.datasource.repository.UserLocalDataSource
 import com.picpay.desafio.android.contacts.datasource.repository.UserRemoteDataSource
 import com.picpay.desafio.android.contacts.datasource.repository.UsersRepository
 import com.picpay.desafio.android.contacts.datasource.repository.impl.UsersRepositoryImpl
 import com.picpay.desafio.android.contacts.datasource.usecase.di.featureContactsUseCasesModule
 import com.picpay.desafio.android.contacts.viewmodel.ContactsScreenViewModel
-import org.koin.androidx.viewmodel.dsl.viewModel
+import com.picpay.desafio.android.network.services.UserService
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val featureContactsModule = module {
 
-    viewModel { ContactsScreenViewModel(get(), get(), get()) }
+    viewModelOf(::ContactsScreenViewModel)
 
     single<UserService> { get<Retrofit>().create(UserService::class.java) }
 
