7180f586ae4bd44bd38ccdbf3be98c6777a42aaf
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-25 22:05:28 -0300

    test: adds user service unit tests

---
+++ core/network/src/test/kotlin/com/picpay/desafio/android/core/network/services/UserService.kt
@@ -0,0 +1,57 @@
+package com.picpay.desafio.android.core.network.services
+
+import com.picpay.desafio.android.core.network.model.UserResponse
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.ResponseBody
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Response
+
+class UserServiceTest {
+
+    private lateinit var userService: UserService
+
+    @Before
+    fun setUp() {
+        userService = mockk()
+    }
+
+    @Test
+    fun `getUsers returns successful response`() = runBlocking {
+        // Given
+        val expectedUsers = listOf(
+            UserResponse("img_url", "User1", 1, "user1"),
+            UserResponse("img_url", "User2", 2, "user2")
+        )
+        coEvery { userService.getUsers() } returns Response.success(expectedUsers)
+
+        // When
+        val response = userService.getUsers()
+
+        // Then
+        assertEquals(true, response.isSuccessful)
+        assertEquals(expectedUsers, response.body())
+    }
+
+    @Test
+    fun `getUsers returns error response`() = runBlocking {
+        // Given
+        val responseBody = mockk<ResponseBody>()
+        every { responseBody.contentType() } returns "application/json".toMediaType()
+        every { responseBody.contentLength() } returns 0L
+        every { responseBody.string() } returns "{}"
+        coEvery { userService.getUsers() } returns Response.error(404, responseBody)
+
+        // When
+        val response = userService.getUsers()
+
+        // Then
+        assertEquals(false, response.isSuccessful)
+        assertEquals(404, response.code())
+    }
+}
