fb2747aa1d7713f952741b9ad19bf213627e7d66
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-25 22:02:29 -0300

    refactor: adds data store repository injection

--- core/datastore/src/main/kotlin/com/picpay/desafio/android/core/datastore/di/datastoreModule.kt
+++ core/datastore/src/main/kotlin/com/picpay/desafio/android/core/datastore/di/datastoreModule.kt
@@ -1,15 +1,18 @@
 package com.picpay.desafio.android.core.datastore.di
 
+import com.picpay.desafio.android.core.datastore.DataStoreRepository
 import com.picpay.desafio.android.core.datastore.DesafioAppDataStoreRepository
 import com.picpay.desafio.android.core.datastore.DesafioAppPreferencesDataSource
 import com.picpay.desafio.android.core.datastore.PreferencesDataSource
-import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val dataStoreModule = module {
 
-    single { DesafioAppDataStoreRepository(androidContext()) }
+    singleOf(::DesafioAppDataStoreRepository)
 
-    single<PreferencesDataSource> { DesafioAppPreferencesDataSource(get()) }
+    singleOf(::DesafioAppPreferencesDataSource) { bind<PreferencesDataSource>() }
 
+    singleOf(::DesafioAppDataStoreRepository) { bind<DataStoreRepository>() }
 }
