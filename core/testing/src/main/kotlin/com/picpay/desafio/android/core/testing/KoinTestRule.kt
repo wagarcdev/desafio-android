d2f665ef29ec20a5ff04e68fd82b1360eb765f5f
Wagner Arcieri <wagner.arcieri@gmail.com>
2024-08-30 14:05:34 -0300

    fix: koin subsequent instrumented tests failing
    #Issue 1557
    (https://github.com/InsertKoinIO/koin/issues/1557)

--- core/testing/src/main/kotlin/com/picpay/desafio/android/core/testing/KoinTestRule.kt
+++ core/testing/src/main/kotlin/com/picpay/desafio/android/core/testing/KoinTestRule.kt
@@ -4,21 +4,27 @@ import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.GlobalContext.getKoinApplicationOrNull
 import org.koin.core.context.GlobalContext.startKoin
-import org.koin.core.context.GlobalContext.stopKoin
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
 
 class DesafioAppKoinTestRule(
     private val modules: List<Module>
 ) : TestWatcher() {
     override fun starting(description: Description) {
-        startKoin {
-            androidContext(InstrumentationRegistry.getInstrumentation().targetContext.applicationContext)
-            modules(modules)
+        if (getKoinApplicationOrNull() == null) {
+            startKoin {
+                androidContext(InstrumentationRegistry.getInstrumentation().targetContext.applicationContext)
+                modules(modules)
+            }
+        } else {
+            loadKoinModules(modules)
         }
     }
 
     override fun finished(description: Description) {
-//        stopKoin()
+        unloadKoinModules(modules)
     }
 }
